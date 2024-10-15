ea2585417f7b2e81f5df5f8a89b7ce785508e5e5
muhammadumair78 <muhammadumairjavaid@gmail.com>
2024-10-15 04:12:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('Machinelearning App')
 st.info('This is the machine learning app!')
 
-with st.expander('Data')
+with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df

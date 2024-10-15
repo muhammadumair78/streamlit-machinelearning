dc13740f823309b9cb8b950ce97f84f2ae8574c0
muhammadumair78 <muhammadumairjavaid@gmail.com>
2024-10-15 05:34:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ with st.expander('Data'):
 with st.expander('Data Visualizations'):
     st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
-with st.sidebar():
+with st.sidebar:
     st.header('Input Fearures')
     island = st.selectbox('Island', ('Torgersen', 'Biscoe', 'Dream'))
     gender = st.selectbox('Gender', ('male', 'female'))

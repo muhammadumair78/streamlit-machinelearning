24e66ca2f4d05a250556fa18b0949df81b6062a7
muhammadumair78 <muhammadumairjavaid@gmail.com>
2024-10-15 05:59:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,12 @@ with st.expander('Data'):
   df
 
   st.write('**X**')
-  X = df.drop('species', axis=1)
-  X
+  X_raw = df.drop('species', axis=1)
+  X_raw
   
   st.write('**Y**')
-  Y = df.species
-  Y
+  Y_raw = df.species
+  Y_raw
 
 with st.expander('Data Visualizations'):
     st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
@@ -39,12 +39,7 @@ with st.sidebar:
     }
 
     input_df = pd.DataFrame(data, index=[0])
-    input_penguins = pd.concat([input_df, X], axis=0)
-    
-    # Encode
-    encode = ['island', 'sex']
-    df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-    input_row = df_penguins[:1]
+    input_penguins = pd.concat([input_df, X_raw], axis=0)
 
 with st.expander('Input Features'):
   st.write('**Input Penguin**')
@@ -54,4 +49,21 @@ with st.expander('Input Features'):
   st.write('**Input Row**')
   input_row
 
+# Encode x
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+input_row = df_penguins[:1]
+
+# Encode y
+target_mapper = {
+  "Adelie": 0,
+  "Chinstrap': 1,
+  "Gentoo": 2,
+}
+
+def target_encode(val):
+  return target_mapper[val]
+
+y = Y_raw.apply(target_encode)
+y
 
